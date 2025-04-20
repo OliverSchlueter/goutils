b471cd25688f49393885489ca1d50ebf5d55ec82
Oliver <oschi1303@gmail.com>
2025-04-20 21:35:00 +0200

    Add package documentation for cloudevents

--- cloudevents/cloudevents.go
+++ cloudevents/cloudevents.go
@@ -1,3 +1,4 @@
+// Package cloudevents is based on the CloudEvents specification: https://github.com/cloudevents/spec
 package cloudevents
 
 import "time"
@@ -5,12 +6,11 @@ import "time"
 const SpecVersion = "1.0.2"
 
 type CloudEvent struct {
-	SpecVersion     string      `json:"specversion"`
-	ID              string      `json:"id"`
-	Type            string      `json:"type"`
-	Subject         string      `json:"subject"`
-	Source          string      `json:"source"`
-	Time            time.Time   `json:"time"`
-	DataContentType string      `json:"datacontenttype"`
-	Data            interface{} `json:"data"`
+	SpecVersion     string    `json:"specversion"`
+	ID              string    `json:"id"`
+	Type            string    `json:"type"`
+	Subject         string    `json:"subject"`
+	Source          string    `json:"source"`
+	Time            time.Time `json:"time"`
+	DataContentType string    `json:"datacontenttype"`
 }
