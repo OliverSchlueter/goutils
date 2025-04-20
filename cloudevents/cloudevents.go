6832a90b61ae695b29289cfd95ebecfdbbb4bb99
Oliver <oschi1303@gmail.com>
2025-04-20 21:05:11 +0200

    Add cloudevents

---
+++ cloudevents/cloudevents.go
@@ -0,0 +1,16 @@
+package cloudevents
+
+import "time"
+
+const SpecVersion = "1.0.2"
+
+type CloudEvent struct {
+	SpecVersion     string      `json:"specversion"`
+	ID              string      `json:"id"`
+	Type            string      `json:"type"`
+	Subject         string      `json:"subject"`
+	Source          string      `json:"source"`
+	Time            time.Time   `json:"time"`
+	DataContentType string      `json:"datacontenttype"`
+	Data            interface{} `json:"data"`
+}
