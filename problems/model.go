f2868e3c5f683b46730403303698f5ab628549ee
Oliver <oschi1303@gmail.com>
2025-06-07 16:23:24 +0200

    Add WriteToBroker method to Problem for publishing to broker

--- problems/model.go
+++ problems/model.go
@@ -2,6 +2,9 @@ package problems
 
 import (
 	"encoding/json"
+	"github.com/OliverSchlueter/goutils/broker"
+	"github.com/OliverSchlueter/goutils/sloki"
+	"log/slog"
 	"net/http"
 	"time"
 )
@@ -15,15 +18,29 @@ type Problem struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
-func (p *Problem) Send(w http.ResponseWriter) {
+func (p *Problem) WriteToHTTP(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/problem+json")
 	w.WriteHeader(p.Status)
 
 	data, err := json.Marshal(p)
 	if err != nil {
+		slog.Warn("failed to marshal problem response", sloki.WrapError(err))
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	_, _ = w.Write(data)
+}
+
+func (p *Problem) WriteToBroker(b broker.Broker, subj string) {
+	data, err := json.Marshal(p)
+	if err != nil {
+		slog.Warn("failed to marshal problem response", sloki.WrapError(err))
+		return
+	}
+
+	if err := b.Publish(subj, data); err != nil {
+		slog.Error("failed to publish problem response", sloki.WrapError(err), "subject", subj)
+		return
+	}
 }
